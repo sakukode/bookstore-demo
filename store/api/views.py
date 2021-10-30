a88e861db4421bea73f5c15bd730e7992f6fb61d
Rizqi Maulana <sakukode@gmail.com>
2021-10-30 19:51:11 +0700

    Dasar Django Rest Framework - Filtering

--- store/api/views.py
+++ store/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from store.models import Category
 
@@ -8,3 +9,11 @@ from .serializers import ExampleSerializer
 class ExampleListView(ListAPIView):
     serializer_class = ExampleSerializer
     queryset = Category.objects.all()
+    search_fields = ['name']
+    ordering_fields = ['name']
+
+    filter_backends = (
+        SearchFilter,
+        OrderingFilter
+    )
+
