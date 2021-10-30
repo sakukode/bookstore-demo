e68edfc39adfa2453f492f7720d574f685138991
Rizqi Maulana <sakukode@gmail.com>
2021-10-30 19:20:45 +0700

    Stud Kasus Dashboard - Manajemen Order

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext
+from django.urls import path
+from functools import update_wrapper
 
 from .forms import ShopAdminForm
-from .models import Category, Shop, Product
+from .models import Category, Shop, Product, Order, OrderProduct
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -57,3 +59,64 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Product, ProductAdmin)
+
+
+class OrderProductAdminInline(admin.TabularInline):
+    model = OrderProduct
+    fields = ('product', 'quantity', 'price', 'weight', 'total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('custom_invoice_number', 'customer_name', 'status', 'shipping_tracking_number', 'payment_method',
+                    'shipping_courier', 'sub_total', 'total_shipping', 'total',)
+    list_filter = ('status', 'payment_method', 'shipping_courier',)
+    list_editable = ('status', 'shipping_tracking_number',)
+    search_fields = ('invoice_number', 'customer_name',)
+    fields = (
+        'invoice_number', 'purchased_at', 'status', 'customer_name', 'customer_phone', 'customer_address',
+        'customer_city',
+        'customer_state', 'customer_postal_code', 'payment_method', 'shipping_courier', 'shipping_tracking_number',
+        'sub_total', 'total_shipping', 'total',)
+    inlines = (OrderProductAdminInline,)
+
+    def custom_invoice_number(self, obj):
+        return mark_safe('<a href="{id}/detail/">{number}</a>'.format(id=obj.id, number=obj.invoice_number))
+
+    custom_invoice_number.short_description = gettext('invoice number')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        if getattr(self, '__detail_view', None):
+            return False
+        return super().has_change_permission(request, obj)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def detail_view(self, request, object_id, form_url='', extra_context=None):
+        setattr(self, '__detail_view', True)
+        # Custom template for detail view
+        self.change_form_template = self.change_form_template
+        ret = self.changeform_view(request, object_id, form_url, extra_context)
+        delattr(self, '__detail_view')
+        return ret
+
+    def get_urls(self):
+        urls = super().get_urls()
+
+        # add detail-view for the object
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+
+            wrapper.model_admin = self
+            return update_wrapper(wrapper, view)
+
+        # for the detail view.
+        urls[len(urls) - 1] = path('<path:object_id>/detail/', wrap(self.detail_view), name='store_order_detail')
+        return urls
+
+
+admin.site.register(Order, OrderAdmin)
--- store/models.py
+++ store/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils.translation import gettext
 from django.utils.text import slugify
 
+from django.contrib.auth.models import User
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name=gettext('name'))
 
@@ -89,3 +92,98 @@ class Product(models.Model):
         self.slug = slugify(self.name)
 
         super(Product, self).save(*args, **kwargs)
+
+
+class Order(models.Model):
+    class Meta:
+        db_table = 'orders'
+        ordering = ('created_at',)
+        verbose_name = gettext('order')
+        verbose_name_plural = gettext('orders')
+
+    MANUAL_PAYMENT = 'manual'
+    ONLINE_PAYMENT = 'online'
+
+    PAYMENT_METHOD_CHOICES = [
+        (MANUAL_PAYMENT, gettext('manual transfer bank')),
+        (ONLINE_PAYMENT, gettext('online')),
+    ]
+
+    JNE_COURIER = 'jne'
+    TIKI_COURIER = 'tiki'
+    POS_INDONESIA_COURIER = 'pos_indonesia'
+
+    SHIPPING_COURIER_CHOICES = [
+        (JNE_COURIER, 'JNE'),
+        (TIKI_COURIER, 'Tiki'),
+        (POS_INDONESIA_COURIER, 'Pos Indonesia'),
+    ]
+
+    PENDING_STATUS = 0
+    PAID_STATUS = 1
+    SHIPPED_STATUS = 2
+    COMPLETED_STATUS = 3
+    CANCELED_STATUS = 4
+
+    STATUS_CHOICES = [
+        (PENDING_STATUS, gettext('pending')),
+        (PAID_STATUS, gettext('paid')),
+        (SHIPPED_STATUS, gettext('shipped')),
+        (COMPLETED_STATUS, gettext('completed')),
+        (CANCELED_STATUS, gettext('canceled')),
+    ]
+
+    invoice_number = models.CharField(max_length=11, verbose_name=gettext('invoice number'))
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # Payment method fields
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHOD_CHOICES, verbose_name=gettext('payment method'))
+    payment_proof = models.ImageField(upload_to='images', null=True, blank=True, verbose_name=gettext('proof of payment'))
+    payment_token = models.CharField(max_length=255, null=True, blank=True, verbose_name=gettext('payment token'))
+    # Shipping courier fields
+    shipping_courier = models.CharField(max_length=20, choices=SHIPPING_COURIER_CHOICES, verbose_name=gettext('shipping courier'))
+    shipping_service = models.CharField(max_length=100, verbose_name=gettext('shipping service'))
+    shipping_tracking_number = models.CharField(max_length=255, null=True, blank=True, verbose_name=gettext('tracking number'))
+    # Customer Info fields
+    customer_name = models.CharField(max_length=100, verbose_name=gettext('customer name'))
+    customer_phone = models.CharField(max_length=20, verbose_name=gettext('customer phone'))
+    customer_address = models.TextField(verbose_name=gettext('customer address'))
+    customer_city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, verbose_name=gettext('customer city'))
+    customer_state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, verbose_name=gettext('customer state'))
+    customer_postal_code = models.CharField(max_length=5, null=True, blank=True, verbose_name=gettext('customer postal code'))
+    # Date fields
+    purchased_at = models.DateTimeField(blank=True, null=True, verbose_name=gettext('Purchased date'))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # Status fields
+    status = models.IntegerField(choices=STATUS_CHOICES, default=PENDING_STATUS, verbose_name=gettext('status'))
+    # Billing fields
+    sub_total = models.FloatField(verbose_name=gettext('sub total'))
+    total_shipping = models.FloatField(verbose_name=gettext('shipping rate'))
+    total = models.FloatField(verbose_name=gettext('total'))
+
+    def __str__(self):
+        return self.invoice_number
+
+    def save(self, *args, **kwargs):
+        if self.shipping_tracking_number:
+            if self.shipping_tracking_number is not None:
+                self.status = self.SHIPPED_STATUS
+
+        super(Order, self).save(*args, **kwargs)
+
+
+class OrderProduct(models.Model):
+    class Meta:
+        db_table = 'order_products'
+        ordering = ['id']
+
+    quantity = models.IntegerField(verbose_name=gettext('quantity'))
+    weight = models.FloatField(verbose_name=gettext('weight'))
+    price = models.FloatField(verbose_name=gettext('price'))
+    total = models.FloatField(verbose_name=gettext('total'))
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name=gettext('order'))
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, verbose_name=gettext('product'))
+
+    def __str__(self):
+        return ''
