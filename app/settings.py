2e0f79ffbeac1276bc4003d792e6b34fec5425f9
Rizqi Maulana <sakukode@gmail.com>
2021-10-31 09:45:03 +0700

    Deployment - Setting Cors

--- app/settings.py
+++ app/settings.py
@@ -47,11 +47,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # cors headers middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -177,3 +179,5 @@ EMAIL_PORT = '<sendinblue_port>'
 
 ADMIN_EMAIL = '<your_email>'
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = []
